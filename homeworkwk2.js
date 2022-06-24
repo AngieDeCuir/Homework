b89cf8cd6a874d8dd5c80849fe80897b20284b54
Angie DeCuir <adecuir8@gmail.com>
2022-06-24 10:29:23 -0500

    week 2 homework merge

--- homeworkwk2.js
+++ homeworkwk2.js
@@ -1,12 +1,13 @@
 //Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
 let  pizzaPlace = "Pizza Planet";
-let  numberOfToppings = "30";
-//Print the variables and their types.
-console.log(typeof 'pizzaPlace');
-console.log(typeof 'numberOfToppings');
-
+let  numberOfToppings = 15;
+//Print the variables and their  types.
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+console.log(pizzaPlace);
+console.log(numberOfToppings);
 //Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
-console.log(`"Welcome to Pizza Planet!" "We have 30 toppings you can choose from!"`);
+console.log(`Welcome to ${pizzaPlace}!" "We have ${numberOfToppings} toppings you can choose from!`);
 //Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
 
 if (numberOfToppings <10){
@@ -15,7 +16,7 @@ if (numberOfToppings <10){
   console.log("A whole lot of pizza.");
 }
 //Use a loop to print all of the even numbers from 1 to numberOfToppings (or 10)
-for(let i = 2; i <= 30; i += 2) {
+for(let i = 2; i <= numberOfToppings; i += 2) {
   console.log(i);
 }
 
