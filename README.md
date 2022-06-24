b9f686c4d94c97814ac04bc6fefa59b654775fd2
Angie DeCuir <adecuir8@gmail.com>
2022-06-24 07:20:50 -0500

    Corrected homework

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-#Angie DeCuir#
-###I am learning full stack web development at Savvy Coders!###
-####My Goals For This Course###
-*To Learn Full Stack Web Development
-*To be Certified in Agile
-*To be Certified in Scrum
-*To create a resume for a job in web development
-*To create a Capstone project to present at Demo Day
-*To complete the program and to start a new career as a web developer
+# Angie DeCuir #
+### I am learning full stack web development at Savvy Coders! ###
+#### My Goals For This Course ###
+* To Learn Full Stack Web Development
+* To be Certified in Agile
+* To be Certified in Scrum
+* To create a resume for a job in web development
+* To create a Capstone project to present at Demo Day
+* To complete the program and to start a new career as a web developer
--- homeworkwk2.js
+++ homeworkwk2.js
@@ -2,11 +2,11 @@
 let  pizzaPlace = "Pizza Planet";
 let  numberOfToppings = "30";
 //Print the variables and their types.
-console.log(pizzaPlace);
-console.log(numberOfToppings);
+console.log(typeof 'pizzaPlace');
+console.log(typeof 'numberOfToppings');
 
 //Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
-`"Welcome to Pizza Planet!" "We have 30 toppings you can choose from!"`
+console.log(`"Welcome to Pizza Planet!" "We have 30 toppings you can choose from!"`);
 //Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
 
 if (numberOfToppings <10){
@@ -15,7 +15,8 @@ if (numberOfToppings <10){
   console.log("A whole lot of pizza.");
 }
 //Use a loop to print all of the even numbers from 1 to numberOfToppings (or 10)
-for (let i = 2; i <= 10; 2+i++){
+for(let i = 2; i <= 30; i += 2) {
   console.log(i);
 }
-//I know it is not correct but I wanted to know how to fix it
+
+//This works but I am not entirely sure why
